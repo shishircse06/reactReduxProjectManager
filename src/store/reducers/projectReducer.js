db1fbc03a61ad179200710ff684aebf8a6e02cca
Shishir <advance.shishir@gmail.com>
2020-01-27 21:48:54 +0600

    project list from redux store

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+	projects:[
+		{'id': 1, 'title': 'first project', 'content':'first project content'},
+		{'id': 2, 'title': 'second project', 'content':'second project content'},
+		{'id': 3, 'title': 'third project', 'content':'second project content'}
+	]
+}
 const projectReducer = (state = initState, action) => {
 	return state
 }
