3a47712acc124ab1d3f8b456b4c4c6bebaca743c
Shishir <advance.shishir@gmail.com>
2020-01-28 14:17:24 +0600

    thunk setup

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -6,6 +6,10 @@ const initState = {
 	]
 }
 const projectReducer = (state = initState, action) => {
+	switch(action.type){
+		case 'CREATE_PROJECT':
+			console.log('project creation', action.project)
+	}
 	return state
 }
 
