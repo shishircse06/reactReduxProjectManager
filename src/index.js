b72b787e876790b88399405731004fac782f688d
Shishir <advance.shishir@gmail.com>
2020-02-03 17:03:39 +0600

    firebase auth ready

--- src/index.js
+++ src/index.js
@@ -16,14 +16,17 @@ import firebaseConfig from './config/firebaseConfig'
 const store = createStore(rootReducer,
   compose(
     applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-    reactReduxFirebase(firebaseConfig), // redux binding for firebase
+    reactReduxFirebase(firebaseConfig, {attachAuthIsReady: true}), // redux binding for firebase
     reduxFirestore(firebaseConfig) // redux bindings for firestore
   )
 );
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+store.firebaseAuthIsReady.then(()=>{
+	ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+	// If you want your app to work offline and load faster, you can change
+	// unregister() to register() below. Note this comes with some pitfalls.
+	// Learn more about service workers: https://bit.ly/CRA-PWA
+	serviceWorker.unregister();
+});
+
