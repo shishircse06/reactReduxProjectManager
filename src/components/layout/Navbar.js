360d1c6d8ccd8ec6cf08560b21d9a97f25eafec0
Shishir <advance.shishir@gmail.com>
2020-02-03 14:58:54 +0600

    user logging in

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import SignInLinks from './SignInLinks'
 import SignOutLinks from './SignOutLinks'
@@ -16,4 +17,10 @@ const Navbar = () => {
 		)
 }
 
-export default Navbar
+const mapStateToProps = (state) => {
+	console.log(state)
+	return {
+
+	}
+}
+export default connect(mapStateToProps)(Navbar)
