34cd08d8e6df5361807369a7e01d9b37b0ec9c1f
Shishir <advance.shishir@gmail.com>
2020-02-03 14:21:38 +0600

    project details from firebase

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyBVtKAX31kxfXmp_uoKDYPMV5sC9kBJpCM",
+  authDomain: "react-redux-firebase-82e7a.firebaseapp.com",
+  databaseURL: "https://react-redux-firebase-82e7a.firebaseio.com",
+  projectId: "react-redux-firebase-82e7a",
+  storageBucket: "react-redux-firebase-82e7a.appspot.com",
+  messagingSenderId: "1053756630097",
+  appId: "1:1053756630097:web:87900ac2546b33bc420470",
+  measurementId: "G-VJCDKNMBWX"
 };
 
 firebase.initializeApp(firebaseConfig)
